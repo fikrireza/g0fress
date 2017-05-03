81499ec0297ce019856dab60498f28eff73abb6e
dessurya <fourline66@gmail.com>
2017-05-03 16:27:32 +0700

    continue frontend page program events css html

---
+++ frontend/js/produk-index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	$(".slider-product").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      items: 1,
+      singleItem:true,
+      pagination:false,
+      navigationText : ["<i class='fa fa-chevron-left' aria-hidden='true'></i><i class='fa fa-chevron-left' aria-hidden='true'></i>","<i class='fa fa-chevron-right' aria-hidden='true'></i><i class='fa fa-chevron-right' aria-hidden='true'></i>"]
+ 
+    });
+    
+})
