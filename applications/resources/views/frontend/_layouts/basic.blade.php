f0bd94932c0494b74ff3576827b1b2bae930eedd
fikrireza <fikrirezaa@gmail.com>
2017-05-16 11:39:38 +0700

    wmt and ga

--- applications/resources/views/frontend/_layouts/basic.blade.php
+++ applications/resources/views/frontend/_layouts/basic.blade.php
@@ -11,7 +11,7 @@
 
 	@yield('meta')
 
-	<meta name="google-site-verification" content="" />
+	<meta name="google-site-verification" content="WoEFPm5mEA4IkWE-EDSpSDrG7zg1ETt3LppVMm5Y5Mg" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="robots" content="index, follow" />
 	<meta name="googlebot" content="all" />
@@ -28,16 +28,16 @@
 	<link rel="icon" type="image/png" href="{{ asset('public/image/default/logo-gofress.png') }}" />
 	<link rel="image_src" href="" />
 
-    <script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'USER_ID', 'gofress.co.id');
-	  ga('require', 'linkid', 'linkid.js');
-	  ga('send', 'pageview');
-    </script>
+		  ga('create', 'UA-99240714-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
 </head>
 <body>
 
