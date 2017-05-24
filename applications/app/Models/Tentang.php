8296c8660d8c9bf29ad5a9108d8496599584364d
fikrireza <fikrirezaa@gmail.com>
2017-05-24 10:16:26 +0700

    remove img in about

--- applications/app/Models/Tentang.php
+++ applications/app/Models/Tentang.php
@@ -8,7 +8,7 @@ class Tentang extends Model
 {
     protected $table = 'amd_tentang';
 
-    protected $fillable = ['deskripsi_ID', 'deskripsi_EN', 'visi_deskripsi_ID', 'visi_deskripsi_EN', 'misi_deskripsi_ID', 'misi_deskripsi_EN', 'img_url', 'img_alt', 'slug', 'actor'];
+    protected $fillable = ['deskripsi_ID', 'deskripsi_EN', 'visi_deskripsi_ID', 'visi_deskripsi_EN', 'misi_deskripsi_ID', 'misi_deskripsi_EN', 'slug', 'actor'];
 
 
 }
